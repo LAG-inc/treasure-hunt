6f48fbfb3d7f67f799cbd981857ee1351f976ebb
Ricardo <05rungr@gmail.com>
2020-10-03 14:30:34 -0500

    Camara corregida

--- Assets/_Scripts/Camera.cs
+++ Assets/_Scripts/Camera.cs
@@ -8,21 +8,22 @@ public class Camera : MonoBehaviour
     private float smoothTime;
 
     private Vector3 _velocity;
-    [SerializeField] private GameObject limitTop, limitBot;
-    [SerializeField] private float offsetSmooth;
+    [SerializeField] private Transform limitTop, limitBot;
     private float _direction;
 
     private void Update()
     {
-        if (target.transform.position.y >= limitTop.transform.position.y ||
-            target.transform.position.y <= limitBot.transform.position.y)
+        if (target.transform.position.y >= limitTop.position.y ||
+            target.transform.position.y <= limitBot.position.y)
         {
             // transform.position = Vector3.Lerp(transform.position,
             //     new Vector3(transform.position.x, target.position.y, -10f),
             //     camVelocity * Time.deltaTime);
 
             transform.position = Vector3.SmoothDamp(transform.position,
-                new Vector3(transform.position.x, target.position.y + offsetSmooth, transform.position.z),
+                new Vector3(transform.position.x,
+                    target.position.y,
+                    transform.position.z),
                 ref _velocity,
                 smoothTime - PlayerController.SI.GetCurrentVelocity());
         }

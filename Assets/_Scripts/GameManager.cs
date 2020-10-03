c8637a7c9914d22c0b8dd7a3e1c1caa2228d2319
Daniel Luque <danielluque14@gmail.com>
2020-10-03 19:27:41 +0200

    Añade Coin y el GameManager
    GameManager:
    Cuando carga una scene de gameplay el GameManager buscará el número de moendas del nivel
    para saber cual es el maximo de monedas en el nivel. Una vez que el jugador agarra una moneda
    esta se desactiva y aumentará el contador de monedas recolectadas. Una vez se
    consigue reunir todas las monedas del nivel, el GameManager podrá reaccionar gracias a los
    eventos de Unity.
    
    Coin:
    Tienen dos collider:
    
      1. En el objeto padre Coin, se encarga de revisar la interactividad con la moneda.
      2. SoundTrigger permitira reproducir sonido o lanzar eventos, una vez el jugador entre
         en el área de la moneda.
    
    Una vez el jugador interactua con la moneda, esta se desactiva.

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+[Serializable]
+public class OnWin : UnityEvent
+{
+}
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager SI;
+
+    [SerializeField]
+    private int coinsInLevel;
+    [SerializeField]
+    private int collectedCoins = 0;
+
+    public OnWin onWin;
+
+    void Awake()
+    {
+        SI = SI == null ? this : SI;
+        coinsInLevel = GameObject.FindObjectsOfType(typeof(Coin)).Length;
+    }
+
+    void Update()
+    {
+        if (collectedCoins >= coinsInLevel)
+        {
+            Debug.Log("GANASTE WEY!");
+            onWin.Invoke();
+        }
+    }
+
+    public void CollectCoin()
+    {
+        collectedCoins++;
+    }
+}

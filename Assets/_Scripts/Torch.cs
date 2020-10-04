ba0673cacf11946265fd0933362e943337fc9edd
Daniel Luque <danielluque14@gmail.com>
2020-10-04 23:41:12 +0200

    Implementada la antorcha y propuesta de nuevo sistema de iluminación
    - Añadida la antorcha #9
    - Reemplazado el sistema de iluminación basado en Sprite Mask por luces 2D
    - Adaptados los assets ya existentes

---
+++ Assets/_Scripts/Torch.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine;
+
+public class Torch : MonoBehaviour
+{
+    private Light2D _torch;
+    private CircleCollider2D _collider;
+
+    void Awake()
+    {
+        _torch = GetComponent<Light2D>();
+        _collider = GetComponent<CircleCollider2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            _torch.enabled = true;
+            _collider.enabled = false;
+        }
+    }
+}
